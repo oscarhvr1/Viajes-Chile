e2d444efab5326773a7da4527fd2f63b4e60f82a
Tugit config --global user.name Tu <oscar.h.vergarami@gmail.com>
2022-06-30 16:44:39 -0400

    modifique un codigo de mi script, tambien ordene los tooltip de mi navbar

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -15,10 +15,6 @@ $(".card-title").click(function () {
 
 });
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
 
 
 
